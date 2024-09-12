31cd3acf9ffe84608aec614f909c1cd9b96ec133
Ivan Smirnov <rupool.pro@yandex.ru>
2024-09-12 18:37:46 +0300

    fix2

--- src/bitcoin.c
+++ src/bitcoin.c
@@ -113,8 +113,8 @@ out:
 	return val;
 }
 
-// static const char *gbt_req = "{\"method\": \"getblocktemplate\", \"params\": [{\"capabilities\": [\"coinbasetxn\", \"workid\", \"coinbase/append\"], \"rules\" : [\"segwit\"]}]}\n";
-static const char *gbt_req = "{\"method\": \"getblocktemplate\", \"params\": [{\"rules\": [\"segwit\"]}]}\n";
+static const char *gbt_req = "{\"method\": \"getblocktemplate\", \"params\": [{\"capabilities\": [\"coinbasetxn\", \"workid\", \"coinbase/append\"], \"rules\" : [\"segwit\"]}]}\n";
+z
 /* Request getblocktemplate from bitcoind already connected with a connsock_t
  * and then summarise the information to the most efficient set of data
  * required to assemble a mining template, storing it in a gbtbase_t structure */
